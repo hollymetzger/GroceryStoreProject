c2820eb8e4fb45e31280ffd2ee28399a007a54de
Turtlefud12 <bkellogg0422@gmail.com>
2025-03-03 19:04:38 -0500

    Add files via upload
    here is the arrival process class, the customer class, the custom queue class, the job class, and the node class

---
+++ Job.java
@@ -0,0 +1,42 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Job {
+    private double serviceTime;
+
+    public Job(double serviceTime) {
+        this.serviceTime = serviceTime;
+    }
+
+    public double getServiceTime() {
+        return serviceTime;
+    }
+
+    public void setServiceTime(double serviceTime) {
+        this.serviceTime = serviceTime;
+    }
+
+    public static void doUnitTests() {
+        System.out.println("Running Job tests");
+
+        try {
+            File file = new File("Job Test Results.csv");
+            FileWriter writer = new FileWriter(file);
+
+            // Test 1: Job creation and getServiceTime
+            Job job = new Job(2.0);
+            writer.write("Test 1: Job creation and getServiceTime, " + (job.getServiceTime() == 2.0) + "\n");
+
+            // Test 2: setServiceTime
+            job.setServiceTime(3.0);
+            writer.write("Test 2: setServiceTime, " + (job.getServiceTime() == 3.0) + "\n");
+
+            writer.close();
+            System.out.println("File created at " + file.getAbsolutePath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
