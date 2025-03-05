c75fab79f4b8f5ee9ae3d7b7cc99dc51f5d7dbe5
Turtlefud12 <bkellogg0422@gmail.com>
2025-03-05 17:32:10 -0500

    Update Main.java
    i put the little message back in

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Running unit tests");
+        
         // Run unit tests for all classes
         runAllUnitTests();
 
