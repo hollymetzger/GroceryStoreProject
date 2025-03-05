6553623456140e4df59605ed3f2ee1711a9593f7
Turtlefud12 <bkellogg0422@gmail.com>
2025-03-05 17:07:52 -0500

    Update Queue.java
    added in unit tests

--- src/Queue.java
+++ src/Queue.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 public class Queue<T> {
 
     // Private fields
@@ -45,7 +48,41 @@ public class Queue<T> {
     }
 
     public void doUnitTests() {
-        // todo: add tests
+        System.out.println("Running Queue tests");
+
+        try {
+            File file = new File("Queue Test Results.csv");
+            FileWriter writer = new FileWriter(file);
+
+            // Test 1: Queue initialization
+            Queue<String> queue = new Queue<>();
+            writer.write("Test 1: Queue initialization, " + queue.isEmpty() + "\n");
+
+            // Test 2: Enqueue operation
+            queue.enqueue("item1");
+            writer.write("Test 2: Enqueue operation, " + ("item1".equals(queue.head.data)) + "\n");
+
+            // Test 3: Enqueue multiple items
+            queue.enqueue("item2");
+            writer.write("Test 3: Enqueue multiple items, " + ("item2".equals(queue.head.next.data)) + "\n");
+
+            // Test 4: Dequeue operation
+            String dequeuedItem = queue.dequeue();
+            writer.write("Test 4: Dequeue operation, " + ("item1".equals(dequeuedItem)) + "\n");
+
+            // Test 5: Dequeue until empty
+            queue.dequeue();
+            writer.write("Test 5: Dequeue until empty, " + queue.isEmpty() + "\n");
+
+            writer.close();
+            System.out.println("File created at " + file.getAbsolutePath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean isEmpty() {
+        return head == null;
     }
 
 }
